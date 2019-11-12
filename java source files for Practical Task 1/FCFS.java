619024cd41bfe1e4f5f3ff1a030ba2a24d22aa25
legendmcdonald <47138510+legendmcdonald@users.noreply.github.com>
2019-11-12 14:30:38 +0100

    Grantt table working with main class but needs to be fixed.

--- java source files for Practical Task 1/FCFS.java
+++ java source files for Practical Task 1/FCFS.java
@@ -12,7 +12,7 @@ import java.util.Comparator;
 
 import static java.util.Collections.*;
 
-public class FCFS{
+public class FCFS {
 
 
 	// The list of processes to be scheduled
@@ -25,7 +25,6 @@ public class FCFS{
 	}
 
 
-
 	public void run() {
 		// TODO Implement the FCFS algorithm here
         /*loop through the process, sort and the set;
@@ -34,8 +33,8 @@ public class FCFS{
 		-Waiting Time
 */
 
-		for(int i=0; i<processes.size(); i++){
-			Process temp=processes.get(i);
+		for (int i = 0; i < processes.size(); i++) {
+			Process temp = processes.get(i);
 
 			//sorts all the process in ascending order
 			sort(processes, new sort());
@@ -52,30 +51,26 @@ public class FCFS{
 		}
 
 
-
 	}
 
 
-
 	//function to find the waiting time for all processes
-	private int findWaitingTime(Process wt)
-	{
-		if(wt.getTurnaroundTime()<0){
+	private int findWaitingTime(Process wt) {
+		if (wt.getTurnaroundTime() < 0) {
 			System.out.println("Can not calculate Waiting Time!");
 		}
-		return wt.getTurnaroundTime()-wt.getBurstTime();
+		return wt.getTurnaroundTime() - wt.getBurstTime();
 	}
 
 
 	//function to calculate turn around time
-	private int findTurnAroundTime (Process tat)
-	{
+	private int findTurnAroundTime(Process tat) {
 
 		//System.out.println(tat.getCompletedTime() -tat.getArrivalTime());
-		if (tat.getCompletedTime()<0){
+		if (tat.getCompletedTime() < 0) {
 			System.out.println("Can not calculate Turn Around Time!");
 		}
-		return tat.getCompletedTime()-tat.getArrivalTime();
+		return tat.getCompletedTime() - tat.getArrivalTime();
 
 	}
 
@@ -84,47 +79,106 @@ public class FCFS{
 	we check to conditions below.
 	* */
 	private int FCT;
-	private int findCompTime( Process ct)
-	{
+
+	private int findCompTime(Process ct) {
 		if (FCT <= ct.getArrivalTime()) {
-			FCT=ct.getArrivalTime()+ct.getBurstTime();
-		}
-			else{
+			FCT = ct.getArrivalTime() + ct.getBurstTime();
+		} else {
 			FCT = FCT + ct.getBurstTime();
-			}
+		}
 		return FCT;
 	}
 
-	public class sort implements Comparator<Process>
-	{
-				// Used for sorting in ascending order of
-				@Override
-				public int compare(Process p1, Process p2) {
+	public class sort implements Comparator<Process> {
+		// Used for sorting in ascending order of
+		@Override
+		public int compare(Process p1, Process p2) {
 
-					return p1.getArrivalTime()-p2.getArrivalTime();
-				}
-
+			return p1.getArrivalTime() - p2.getArrivalTime();
+		}
 
 
 	}
+
 	public void printTable() {
 		// TODO Print the list of processes in form of a table here
 		System.out.print("-------------------------------------------\n");
-		System.out.println("PID     AT      BT      CT     TAT     WT");
-		for(int i=0; i<processes.size(); i++) {
+		System.out.println("PID     AT      BT       CT     TAT     WT");
+		for (int i = 0; i < processes.size(); i++) {
 			Process temp = processes.get(i);
 
-			System.out.println(" "+temp.getProcessId() + "\t\t" + temp.getArrivalTime()+"\t\t"+ temp.getBurstTime()+"\t\t "
-					+temp.getCompletedTime()+"\t\t"+temp.getTurnaroundTime()+"\t\t"+temp.getWaitingTime());
+			System.out.println(" " + temp.getProcessId() + "\t\t" + temp.getArrivalTime() + "\t\t" + temp.getBurstTime() + "\t\t "
+					+ temp.getCompletedTime() + "\t\t" + temp.getTurnaroundTime() + "\t\t" + temp.getWaitingTime());
 
 		}
 		System.out.print("-------------------------------------------\n");
 	}
-
 
 
 	public void printGanttChart() {
 		// TODO Print the demonstration of the scheduling algorithm using Gantt Chart
+		String Equ="";
+		String slash="";
+		String cpt="";
+
+
+		System.out.println("\n\n%%%%%%%%%%% GRANTT CHART %%%%%%%%%%\n");
+		//
+		for (int i=0; i<processes.size(); i++){
+			Process temp = processes.get(i);
+
+			if(i==0){
+				slash+="|";
+				cpt += "0";
+
+
+
+			}else {
+				if (processes.get(i - 1).completedTime < temp.arrivalTime) {
+
+					int help = temp.getArrivalTime() - processes.get(i - 1).completedTime;
+					for (int j = 0; j < help; j++) {
+
+                        slash = slash + "≠";
+                        cpt = cpt + ' ';
+
+					}
+                    slash = slash + "||";
+                    cpt = cpt + temp.arrivalTime;
+				}
+
+			}
+
+			for (int j=0; j<temp.getBurstTime()+2; j++) {
+				if (j == temp.getBurstTime()/2){
+                    slash = slash + ("  P" + temp.processId);
+
+					j++;
+				}
+                slash = slash + " ";
+                cpt = cpt + " ";
+			}
+
+					if(temp.completedTime<10)  {
+                        cpt = cpt + (temp.completedTime + " ");
+					}   else{
+                        cpt = cpt + ("   " + temp.completedTime);
+					}
+            slash = slash + "||";
+
+			}
+		
+		for(int i=0;i<slash.length();i++){
+            Equ = Equ + "=";
+				}
+
+		System.out.println(Equ);
+		System.out.println(slash);
+		System.out.println(Equ);
+		System.out.println(cpt);
+
+		System.out.println("\n'≠' indicates the CPU is waiting for processes");
+
 
 	}
 }
--- java source files for Practical Task 1/Main.java
+++ java source files for Practical Task 1/Main.java
@@ -6,16 +6,36 @@ public class Main {
 
 
 
-        processes.add(new Process(1, 0, 4));
-        processes.add(new Process(2, 1, 3));
-        processes.add(new Process(3, 2, 1));
-        processes.add(new Process(4, 3, 2));
-        processes.add(new Process(5, 4, 5));
+        /*
+        processes.add(new Process(1, 0, 18));
+        processes.add(new Process(2, 2, 5));
+        processes.add(new Process(3, 4, 7));
+        processes.add(new Process(1, 0, 2));
+        processes.add(new Process(2, 3, 1));
+        processes.add(new Process(3, 5, 6));
 
-        FCFS help = new FCFS(processes);
-        help.run();
-        help.printTable();
-        help.printGanttChart();
+        //test1
+        processes.add(new Process(1, 0, 2));
+        processes.add(new Process(2, 3, 1));
+        processes.add(new Process(3, 5, 6));
+*/
+
+        //test5
+        processes.add(new Process(1, 0, 18));
+        processes.add(new Process(2, 3, 2));
+        processes.add(new Process(3, 25, 5));
+        processes.add(new Process(4, 29, 2));
+        processes.add(new Process(5, 33, 7));
+
+
+
+
+
+
+        FCFS temp = new FCFS(processes);
+        temp.run();
+        temp.printTable();
+        temp.printGanttChart();
 
 
     }
